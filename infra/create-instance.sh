5d67b4cf57b96edee035e8cb4ce2e447624b1e30
mbimbij <joseph.mbimbi@gmail.com>
2021-06-01 22:40:56 +0200

    Add production instance to private subnet

--- infra/create-instance.sh
+++ infra/create-instance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ "$#" -ne 6 ]]; then
-  echo -e "usage:\n./create-execution-environment.sh \$APPLICATION_NAME \$STACK_NAME \$AMI_ID \$KEY_NAME \$ENVIRONMENT \$NETWORK_STACK_NAME"
+if [[ "$#" -ne 7 ]]; then
+  echo -e "usage:\n./create-execution-environment.sh \$APPLICATION_NAME \$STACK_NAME \$AMI_ID \$KEY_NAME \$ENVIRONMENT \$NETWORK_STACK_NAME \$SUBNET_NAME"
   exit 1
 fi
 
@@ -11,6 +11,7 @@ AMI_ID=$3
 KEY_NAME=$4
 ENVIRONMENT=$5
 NETWORK_STACK_NAME=$6
+SUBNET_NAME=$7
 
 echo -e "##############################################################################"
 echo -e "creating environment \"$ENVIRONMENT\""
@@ -24,4 +25,5 @@ aws cloudformation deploy \
     AmiId=$AMI_ID \
     ApplicationName=$APPLICATION_NAME \
     Environment=$ENVIRONMENT \
-    NetworkStackName=$NETWORK_STACK_NAME
+    NetworkStackName=$NETWORK_STACK_NAME \
+    SubnetName=$SUBNET_NAME
